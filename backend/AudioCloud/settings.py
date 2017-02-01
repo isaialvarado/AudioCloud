b52144ea89e58bb54820e4ec216fdd37984e9368
aaduru <ujwala_aaduru@yahoo.com>
2017-02-01 10:40:13 -0800

    update back end

--- backend/AudioCloud/settings.py
+++ backend/AudioCloud/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'django_extensions',
+    'rest_framework_jwt',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
--- backend/audio/urls.py
+++ backend/audio/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.root, name='root'),
-     url(r'^$', views.index, name='index'),
+    url(r'^$', views.root, name='root'),
+    #  url(r'^$', views.index, name='index'),
     url(r'^(?P<id>[0-9]+)/$', views.detail, name='detail'),
 
     url(r'^(?P<id>[0-9]+)/results/$', views.results, name='results'),
